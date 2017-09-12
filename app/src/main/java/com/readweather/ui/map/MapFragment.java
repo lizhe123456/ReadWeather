3cb3c05dc72f6ff04ee0b1f3e6f1133ae2729680
Administrator <603399531@qq.com>
2017-09-12 18:42:18 +0800

    地图

--- app/src/main/java/com/readweather/ui/map/MapFragment.java
+++ app/src/main/java/com/readweather/ui/map/MapFragment.java
@@ -2,19 +2,32 @@ package com.readweather.ui.map;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
 import com.amap.api.location.AMapLocation;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.BitmapDescriptorFactory;
 import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
 import com.readweather.R;
 import com.readweather.app.App;
 import com.readweather.base.BaseFrament;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * author：lizhe
@@ -23,18 +36,39 @@ import butterknife.BindView;
  * 类介绍：
  */
 
-public class MapFragment extends BaseFrament implements App.RWLocationListener{
+public class MapFragment extends BaseFrament implements App.RWLocationListener {
 
 
     @BindView(R.id.map)
     MapView map;
+    @BindView(R.id.search_match)
+    View searchMatch;
+    @BindView(R.id.menu)
+    ImageView menu;
+    @BindView(R.id.edit_search)
+    EditText editSearch;
+    @BindView(R.id.search)
+    ImageView search;
+    @BindView(R.id.search_maps_bar)
+    RelativeLayout searchMapsBar;
+    @BindView(R.id.ori_compass)
+    ImageButton oriCompass;
+    @BindView(R.id.my_location_btn)
+    ImageButton myLocationBtn;
 
     private AMap aMap;
 
     private MyLocationStyle myLocationStyle;
 
+    private UiSettings mUiSettings;
+
+    private MarkerOptions mMarkerOptions;
     //首次定位
     private boolean isFirstLoc = true;
+    //显示信息
+    private String desc = "";
+
+    private Handler handler = new Handler();
 
     @Override
     protected int setLayout() {
@@ -44,11 +78,17 @@ public class MapFragment extends BaseFrament implements App.RWLocationListener{
     @Override
     protected void init() {
         map.onCreate(savedInstanceState);
-        if (aMap == null){
+        if (aMap == null) {
             aMap = map.getMap();
             initMap();
             initLocation();
+            initMarker();
         }
+    }
+
+
+    private void showMark() {
+
     }
 
     @Override
@@ -62,13 +102,29 @@ public class MapFragment extends BaseFrament implements App.RWLocationListener{
 
 
     private void initMap() {
-        myLocationStyle = new MyLocationStyle();
-        aMap.setMyLocationStyle(myLocationStyle);
-        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.fromResource(R.drawable.ic_qu_direction_mylocation));
-        myLocationStyle.strokeColor(Color.BLACK);
-        myLocationStyle.radiusFillColor(Color.argb(100, 0, 0, 180));
         aMap.getUiSettings().setMyLocationButtonEnabled(true);
         aMap.setMyLocationEnabled(true);
+        myLocationStyle = new MyLocationStyle();
+        initUISetting();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory.
+                fromResource(R.drawable.gps_point));
+        myLocationStyle.strokeColor(Color.WHITE);
+        myLocationStyle.radiusFillColor(Color.argb(10, 0, 0, 180));
+        myLocationStyle.strokeWidth(5);
+        // 只定位，不进行其他操作
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);
+        aMap.setMyLocationStyle(myLocationStyle);
+    }
+
+    private void initUISetting() {
+        mUiSettings = aMap.getUiSettings();
+        mUiSettings.setZoomControlsEnabled(false);
+        // 是否显示默认的定位按钮
+        mUiSettings.setMyLocationButtonEnabled(false);
+    }
+
+    private void initMarker() {
+        mMarkerOptions = new MarkerOptions();
     }
 
     @Override
@@ -95,21 +151,22 @@ public class MapFragment extends BaseFrament implements App.RWLocationListener{
     }
 
     @Override
-    public void onDestroy() {
-        super.onDestroy();
-        map.onDestroy();
-    }
-
-    @Override
     public void onLocationChanged(AMapLocation location) {
         if (isFirstLoc) {
 
             //设置缩放级别（缩放级别为4-20级）
-            aMap.moveCamera(CameraUpdateFactory.zoomTo(4));
+            aMap.moveCamera(CameraUpdateFactory.zoomTo(17));
             //将地图移动到定位点
             aMap.moveCamera(CameraUpdateFactory.changeLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+            mMarkerOptions.position(new LatLng(location.getLatitude(), location.getLongitude()));
+            mMarkerOptions.title("西安市");
+            mMarkerOptions.snippet("气泡的文字");
+            mMarkerOptions.perspective(true);
+            mMarkerOptions.draggable(true);
+            mMarkerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_camera_location));//设置图标
+            aMap.addMarker(mMarkerOptions);
             isFirstLoc = false;
-         }else {
+        } else {
             //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
             Log.e("地图错误", "定位失败, 错误码:" + location.getErrorCode() + ", 错误信息:"
                     + location.getErrorInfo());
